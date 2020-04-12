e5c1d8055f846cbee199d3e9338ae83602acf88a
Matthew Croak <matthewcroak@Matthews-MBP.fios-router.home>
2020-04-12 11:21:00 -0400

    first-commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import ScrollArrow from './components/ScrollArrow';
+
+function App() {
+  return (
+    <div className="App">
+      <ScrollArrow/>
+    </div>
+  );
+}
+
+export default App;
