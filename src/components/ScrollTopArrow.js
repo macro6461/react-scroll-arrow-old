b2517a1f3911e01d1b46e5893afc95bae4872943
macro6461 <mattcroak718@gmail.com>
2020-11-07 15:37:25 -0500

    fixed memory leak by incorporating cleanup function

--- src/components/ScrollTopArrow.js
+++ src/components/ScrollTopArrow.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {FaArrowCircleUp} from 'react-icons/fa';
 import '../App.css';
 
@@ -6,6 +6,13 @@ import '../App.css';
 const ScrollTopArrow = () =>{
 
   const [showScroll, setShowScroll] = useState(false)
+
+  useEffect(()=>{
+    window.addEventListener('scroll', checkScrollTop)
+    return function cleanup() {
+      window.removeEventListener('scroll', checkScrollTop)
+    }
+  })
 
   const checkScrollTop = () => {
     if (!showScroll && window.pageYOffset > 400){
@@ -18,8 +25,6 @@ const ScrollTopArrow = () =>{
   const scrollTop = () =>{
     window.scrollTo({top: 0, behavior: 'smooth'});
   };
-
-  window.addEventListener('scroll', checkScrollTop)
 
   return (
         <FaArrowCircleUp className="scrollTop" onClick={scrollTop} style={{height: 40, display: showScroll ? 'flex' : 'none'}}/>
